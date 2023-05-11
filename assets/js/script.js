5ec23ccb62efafa3459bba9d74d4daf4c3b3650a
Higor Costa <higorcosta972@gmail.com>
2023-05-11 19:36:13 -0300

    Armazena elementos HTML em variáveis

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const shortenerContainer = document.querySelector('.shortener');
+const inputUrl = document.querySelector('.shortener__input');
+const messageError = inputUrl.nextElementSibling;
+const shortenButton = document.querySelector('#shortenButton');
