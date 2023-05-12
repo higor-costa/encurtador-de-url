6ddaa8f455327a97d06284cf79acf38f9d7709de
Higor Costa <higorcosta972@gmail.com>
2023-05-12 10:04:01 -0300

    Implementa função para fetch da API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,15 @@ const shortenerContainer = document.querySelector('.section-shorten');
 const inputUrl = document.querySelector('.shortener__input');
 const messageError = inputUrl.nextElementSibling;
 const shortenButton = document.querySelector('#shortenButton');
+
+async function urlShortener() {
+  const url = inputUrl.value;
+
+  try {
+    const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    const json = await response.json();
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
